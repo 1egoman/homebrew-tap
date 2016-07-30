222dc0215ae0e40b69a2d8656db5203cca341a7c
Ryan Gaus <rsg1egoman@gmail.com>
2016-07-30 16:39:48 -0400

    Update biome.rb

--- biome.rb
+++ biome.rb
@@ -1,9 +1,9 @@
 class Biome < Formula
   desc "A shell script to manage your app's environment."
   homepage "https://github.com/1egoman/biome"
-  url "https://github.com/1egoman/biome/archive/v0.6.1.tar.gz"
+  url "https://github.com/1egoman/biome/archive/v0.6.2.tar.gz"
   version "0.6.2"
-  sha256 "35213e176d0179f03b7d8eeed22d21aa0d94997b59f90ec0c10ed4a6223c09ee"
+  sha1 "03e22bff1f8750adeda2d8942e02c91c7ffd4f12"
 
   def install
     cp "biome.sh", "biome"
