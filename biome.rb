78f53b7cdf41ad87cdf68e77eef1a08431505f07
Ryan Gaus <rsg1egoman@gmail.com>
2016-10-23 10:02:14 -0400

    Update biome.rb

--- biome.rb
+++ biome.rb
@@ -3,7 +3,7 @@ class Biome < Formula
   homepage "https://github.com/1egoman/biome"
   url "https://github.com/1egoman/biome/archive/0.7.tar.gz"
   version "0.7.0"
-  sha256 "33a28a994dec1a5f4e55cb172bf5a8dc3c62e4f7a5795dfeb8f80cefc858a23a"
+  sha256 "9c9649b3b318758d51264bd8e04361316498e1df0a364b74cedf75fee426820c"
 
   def install
     cp "biome.sh", "biome"
